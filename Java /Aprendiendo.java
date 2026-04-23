5f5ee79600748ddf7a440ad5e7030b5500634f7d
miguelgomz <miguelangelgomezmiranda06@gmail.com>
2026-04-23 09:04:05 +0200

    Aprendi asignar valor a un objeto

---
+++ Java /Aprendiendo.java
@@ -0,0 +1,19 @@
+ class Prueba {
+    String texto_objeto;
+    int numero_objeto;
+}
+public class Aprendiendo {
+    public static void main(String[] args) {
+
+        Prueba mi_objeto = new Prueba();
+
+
+        mi_objeto.texto_objeto="Hola est es el contendio texto de mi objeto ";
+        mi_objeto.numero_objeto=550;
+    System.out.println("Hola " + mi_objeto.texto_objeto);
+    System.out.println("Hola " + mi_objeto.numero_objeto);
+
+
+        
+    }
+}
