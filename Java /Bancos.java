27a5162dc6e7c84096fc10a3805e8fd6c28362f8
miguelgomz <miguelangelgomezmiranda06@gmail.com>
2026-04-23 08:36:03 +0200

    Estos dos codigos no sirven pero me enseñaron

---
+++ Java /Bancos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Banca{
+
+
+public Banca[] nombres(){
+
+
+    Banca [] nombre_usuarios={
+        new Banca="Pepe";
+        new Banca="Perez";
+        new Banca="laura";
+        new Banca="Juan";
+        new Banca="Andres";
+        new Banca="Pedro";
+        new Banca="Penelope";
+    }
+    return nombre_usuarios;
+}
+}
+
+
+    public class Bancos{
+        public static void main(String[] args) {
+           
+        }
+    }
